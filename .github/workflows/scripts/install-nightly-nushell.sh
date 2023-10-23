9622552ba6c126b0adef6aabe82deb8751b5bdb9
amtoine <stevan.antoine@gmail.com>
2023-10-23 19:08:10 +0200

    pass arguments to `install-nightly-nushell.sh`

--- .github/workflows/scripts/install-nightly-nushell.sh
+++ .github/workflows/scripts/install-nightly-nushell.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
+arch="$1"
+ext="$2"
+nu="$3"
+
 tarball=$(\
     curl -L https://api.github.com/repos/nushell/nightly/releases \
         | jq 'sort_by(.published_at) | reverse | .[0].assets'\
-        | jq '.[] | select(.name | test("${{ env.ARCH }}.${{ env.EXT }}")) | {name, browser_download_url}'\
+        | jq '.[] | select(.name | test("$arch.$ext")) | {name, browser_download_url}'\
 )
-name=$(echo $tarball | jq '.name' | tr -d '"' | sed 's/.${{ env.EXT }}$//')
+name=$(echo $tarball | jq '.name' | tr -d '"' | sed 's/.$ext$//')
 url=$(echo $tarball | jq '.browser_download_url' | tr -d '"')
 
 curl -fLo $name $url
 
-if [ "${{ env.EXT }}" = "tar.gz" ]; then
+if [ "$ext" = "tar.gz" ]; then
     tar xvf $name --directory /tmp
-elif [ "${{ env.EXT }}" = "zip" ]; then
+elif [ "$ext" = "zip" ]; then
     unzip $name -d "/tmp/$name"
 fi
-cp "/tmp/$name/${{ env.NU_BIN }}" "$HOME/${{ env.NU_BIN }}"
+cp "/tmp/$name/$nu" "$HOME/$nu"
